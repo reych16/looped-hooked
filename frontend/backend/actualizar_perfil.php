7e36000d203eee09b77813cab4786e2b1eb99f98
Priscila <priscilla.rojas.elizondo@email.com>
2026-04-13 16:00:16 -0600

    pequeño cambio a actualizar_perfil.php

--- frontend/backend/actualizar_perfil.php
+++ frontend/backend/actualizar_perfil.php
@@ -2,6 +2,10 @@
 session_start();
 include("backend/config/conexion.php");
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if (!isset($_SESSION['usuario_id'])) {
     header("Location: login.php");
     exit();
